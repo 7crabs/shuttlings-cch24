3c8424c6c670ef322062ec1d8da1d3bdbfb61a5c
nk <41434466+7crabs@users.noreply.github.com>
2024-12-19 22:48:21 +0900

    task2

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
-use axum::{routing::get, Router};
+use axum::{routing::get, Router, http::{header::{self, HeaderMap}, StatusCode}};
 
 async fn hello_world() -> &'static str {
     "Hello, bird!"
 }
 
+async fn seek() -> (StatusCode, HeaderMap) {
+    let mut headers = HeaderMap::new();
+    headers.insert(header::LOCATION, "https://www.youtube.com/watch?v=9Gc4QTqslN4".parse().unwrap());
+    (StatusCode::FOUND, headers)
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
-
+    let router = Router::new()
+        .route("/", get(hello_world))
+        .route("/-1/seek", get(seek));
     Ok(router.into())
 }
